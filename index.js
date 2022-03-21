2ba75bfe94efc9823f89c4cefaebc7a5ac4aca01
amirul-islam <amirulislam.shanto.75@gmail.com>
2022-03-22 00:03:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const app = express();
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.jgifu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("Pro-Care");
+    const doctorCollection = database.collection("doctorCollection");
+    const serviceCollection = database.collection("ServiceCollection");
+    const userCollection = database.collection("UserCollection");
+    const appoinmentCollection = database.collection("appoinmentCollection");
+    const feedbackCollection = database.collection("feedbackCollection");
+
+    // start create api
+    //
+    //
+    //
+    //
+    //
+
+    app.get("/doctors/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await doctorCollection.findOne(query);
+      res.json(result);
+    });
+    // appoinment collection
+    app.post("/appoinment", async (req, res) => {
+      const appoinment = req.body;
+      const result = await appoinmentCollection.insertOne(appoinment);
+      res.json(result);
+    });
+    ///post feedback
+    app.post("/addReview", async (req, res) => {
+      const feedback = req.body;
+      const result = await feedbackCollection.insertOne(feedback);
+      res.json(result);
+    });
+    // post add doctor
+    app.post("/add-doctor", async (req, res) => {
+      const doctor = req.body.newDoctor;
+      const result = await doctorCollection.insertOne(doctor);
+      res.json(result);
+    });
+
+    //    get all data from doctorCollection database
+    app.get("/all-doctor", async (req, res) => {
+      const result = await doctorCollection.find({}).toArray();
+      res.json(result);
+    });
+    app.post("/add-service", async (req, res) => {
+      const service = req.body.newService;
+      const result = await serviceCollection.insertOne(service);
+      res.json(result);
+    });
+
+    //    get all data from serviceCollection database
+    app.get("/all-service", async (req, res) => {
+      const result = await serviceCollection.find({}).toArray();
+      res.json(result);
+    });
+    // save user in database  email and passsword
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+
+      res.json(result);
+    });
+    // save user in database  google login
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    }); // check admin
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      if (email) {
+        const coursor = { email: email };
+        const user = await userCollection.findOne(coursor);
+        let isAdmin = false;
+        if (user?.role === "admin") {
+          isAdmin = true;
+        }
+        res.json({ admin: isAdmin });
+      }
+    });
+    // get all review
+    // app.get("/feedback", async (req, res) => {
+    //   const result = await feedbackCollection.find({}).toArray();
+    //   res.json(result);
+    // });
+
+    //
+    //
+    //
+    //
+    //
+    //
+  } finally {
+    //    await client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello !");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
